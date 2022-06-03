ec699e7c602aea7b8a3c2234e686c34ac04306f7
tcai2000 <tcai2000ely@gmail.com>
2022-06-04 00:15:56 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,8 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
+import {  FontAwesomeIcon  } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faMagnifyingGlass, faSpinner} from '@fortawesome/free-solid-svg-icons';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles)
 
@@ -8,7 +11,19 @@ function Header() {
     return  ( 
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                
+                <img src={images.logo} alt="Tik Tok"/>
+                <div className={cx('search')}>
+                    <input type="text" placeholder='Search accounts and video' spellCheck={false}/>
+                    <button>
+                        <FontAwesomeIcon className={cx('clear')} icon={faCircleXmark} />                        
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />                        
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />      
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
